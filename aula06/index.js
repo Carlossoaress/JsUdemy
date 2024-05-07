b61450ccc81f2c8ddbcfb7366125c9f979230ae2
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-05-06 23:42:43 -0300

    Create index.js

---
+++ aula06/index.js
@@ -0,0 +1,16 @@
+let nome = "joão"; // string
+console.log(nome,'nasceu em 1984.');
+console.log('em 2000',nome,'conheceu Maria');
+console.log(nome,'casou- se com Maria em 2015');
+console.log('Maria teve 1 filho com',nome,'em 2015');
+console.log('o filho de ' , nome, 'se chama eduardo');
+
+/*
+* não podemos criar variaveis com palavras reservadas
+* Variaveis precisam ter nomes significativos
+* Não pode começar o nome de uma Variável com um numero
+* Utilizamos camelCase
+* case-sensitive
+* Não podemos redeclarar váriavel com let
+* Não ultilize var , ultilize LET
+*/
