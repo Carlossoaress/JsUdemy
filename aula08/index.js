155aeb2d02bb9bbbe3282b6d37e976b66ff10f93
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-05-15 19:29:38 -0300

    Create index.js

---
+++ aula08/index.js
@@ -0,0 +1,10 @@
+const nome = 'junior';
+const sobrenome = 'lima';
+const idade = 31;
+const peso = 84;
+const alturaEmMetros = 1.80;
+let imc = peso /(alturaEmMetros * alturaEmMetros);
+let anoNascimento = 2024 - idade;
+console.log(nome,sobrenome,'tem',idade,'anos','pesa',peso,'kg',)
+console.log('tem',alturaEmMetros,'de altura e seu IMC é de',imc,)
+console.log(nome, sobrenome,'nasceu em',anoNascimento);
