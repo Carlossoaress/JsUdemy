71d090ed65faf43c5a037a97499197fb1e392058
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-05-23 15:52:07 -0300

    Update index.js

--- aula09/index.js
+++ aula09/index.js
@@ -5,4 +5,13 @@ console.log(nome);
  let nao pode ser reatribuida da ERROR*/
 
 
- 
+ const nome ='luiz';   // string
+ const nome1 ="luiz";  // string
+ const nome2 =`luiz`;  // string
+ const num1 = 10;  //number
+ const num2 = 10.52; // number
+ let nomeAluno;  // underfined > nao aponta para local nenhum na memoria
+ const sobrenomeAluno = null; // nulo> valor nulo nao aponta para local nenhum na memoria
+ const boolean = true; false; // verdadeiro ou falso
+
+
