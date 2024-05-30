0b1a6039cc86f324b2e8c46594e9ae52f011c089
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-05-30 00:00:59 -0300

    .

---
+++ aula11/js/index.js
@@ -0,0 +1,7 @@
+let num1= prompt('digite um numero');
+let num2= prompt('digite outro numero')
+let resultado;
+num1=Number(num1);
+num2=Number(num2);
+resultado=num1 + num2
+alert(`a soma dos  valores digitados é ${resultado}`);
