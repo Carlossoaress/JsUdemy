0d1d3432818befe210b07160ba991f05879c424e
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-06-20 17:50:47 -0300

    .

---
+++ aula15/ex/js/script.js
@@ -0,0 +1,12 @@
+const numero = Number(prompt('Digite um Número'))
+const numeroTitulo = document.getElementById('numero-titulo');
+const Texto = document.getElementById('texto');
+numeroTitulo.innerHTML = numero; 
+
+Texto.innerHTML = '';
+Texto.innerHTML += `<p>Raiz quadrada: ${numero** 0.5}.</p>`;
+Texto.innerHTML += `<p>É NaN:${Number.isNaN(numero)}</p>`;
+Texto.innerHTML += `<p>${numero} é interiro: ${Number.isInteger(numero)}</p>`;
+Texto.innerHTML += `<p> Arredondando para Baixo:${Math.floor(numero)}</p>`;
+Texto.innerHTML += `<p>Arredondando para Cima: ${Math.ceil(numero)}</p>`;
+Texto.innerHTML += `<p>Com Duas Casas Decimais:${numero.toFixed(2)}</p>`;
