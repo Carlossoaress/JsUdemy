807525e71aaae70eb8918e833c1681453da175cb
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-07-08 20:13:32 -0300

    Update funcao.js

--- aula17/funcao.js
+++ aula17/funcao.js
@@ -4,3 +4,12 @@ return `Bom Dia ${nome}`;
 const variavel = saudacao('Carlos');
 console.log(variavel);*/
 
+function soma(x,y){
+  const resultado = x + y;
+  return resultado;
+};
+console.log(soma(2,5));
+console.log(soma(5,5));
+console.log(soma(10,5));
+console.log(soma(15,5));
+console.log(soma(20,5));
