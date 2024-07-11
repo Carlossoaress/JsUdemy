d4334ec75e90c7afe8ba841dc7ab484ca55676d6
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-07-10 22:49:03 -0300

    Update funcao.js

--- aula17/funcao.js
+++ aula17/funcao.js
@@ -12,6 +12,10 @@ console.log(soma(2,5));
 console.log(soma(5,5));
 console.log(soma(10,5));
 console.log(soma(15,5));
-console.log(soma(20,5));*/
+console.log(soma(22,5));*/
 
-const raiz
+const raiz = function (n){
+  return n**0.5;
+}
+
+console.log(raiz(9));
