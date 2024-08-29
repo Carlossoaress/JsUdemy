beffff8f62ab160a930350b9853e13d440c8262d
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-08-28 23:27:37 -0300

    Create index.js

---
+++ aula27/index.js
@@ -0,0 +1,10 @@
+const pontuacaoUSuario = 999;
+ //if (pontuacaoUSuario >= 1000){
+    //console.log('Usuario Vip');
+ //}else{
+   // console.log('Usuario Comum');
+
+
+   const NIVELuSUARIO = pontuacaoUSuario >=100 ? 'Usuario Vip' : 'Usuario Comum';
+   console.log(NIVELuSUARIO);
+   
