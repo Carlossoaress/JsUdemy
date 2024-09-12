3fcffedfa31f9b936eb66765c69e967050ce0579
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-09-12 10:50:03 -0300

    Update index.js

--- aula28/index.js
+++ aula28/index.js
@@ -1,2 +1,10 @@
 const data = new Date();
+console.log('dia:', data.getDate());
+console.log('mês:', data.getMonth()+1);
+console.log('ano:', data.getFullYear());
+console.log('hora:', data.getHours());
+console.log('min:', data.getMinutes());
+console.log('seg:', data.getSeconds());
+console.log('ms:', data.getMilliseconds());
+console.log('Dia da semana:', data.getDay());
 console.log(data.toString());
