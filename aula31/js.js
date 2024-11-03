a0272cd3e8593d5dd24af5c2995bf560c86022a4
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-11-02 22:27:39 -0300

    Update js.js

--- aula31/js.js
+++ aula31/js.js
@@ -1,6 +1,11 @@
-function falaoi (nome){
+/*function falaoi (nome){
     console.log(`junior ${nome}`);
          
 }
-falaoi('Lima');
+falaoi('Lima');*/
 
+function soma (a,b){
+    const resultado = a+b;
+    return resultado;
+}
+console.log(soma(2,2));
