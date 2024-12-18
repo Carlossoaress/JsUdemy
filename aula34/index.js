6f7864c911ffd37b48371278d35038d24e5fbd98
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2024-12-18 15:53:08 -0300

    aula34
    aula34

--- aula34/index.js
+++ aula34/index.js
@@ -1,1 +1,9 @@
-// estruturas de repetição
+/* estruturas de repetição
+for (let i = 100; i >= 50; i-=10){
+    console.log(`boa noite ${i}`);
+}*/
+
+for (let i = 0; i<= 10;i++){
+    const par =i %2 ===0;
+    console.log(i,par);
+};
