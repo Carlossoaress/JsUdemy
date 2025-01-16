6738dbe720d3373ab4601f0cea32d78ef439f50c
Carlos Soares <119452954+Carlossoaress@users.noreply.github.com>
2025-01-16 20:23:57 -0300

    Create index.js

---
+++ aula36/index.js
@@ -0,0 +1,10 @@
+// FOR IN le os indices
+const frutas = ['pera','maçã','uva'];
+/*for(let i = 0;i < frutas.length;i++){
+    console.log(frutas[i]);
+
+}*/
+
+for (let index in frutas){
+    console.log(frutas[index]);
+}
